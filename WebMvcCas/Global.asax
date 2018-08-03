ced914bf2f133f48b9d9de5701be76671999bb66
tomasJun <1280033048@qq.com>
2018-08-04 02:14:15 +0800

    first commit

---
+++ WebMvcCas/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebMvcCas.MvcApplication" Language="C#" %>
---
+++ WebMvcCas/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebMvcCas.Startup))]
+namespace WebMvcCas
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
